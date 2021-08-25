1ac510cd4a126d7bc5c817a0538f56b66cf55f73
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-08-25 14:08:22 -0300

    exercicio 3

--- Linguagem de programação/aula_02/exercicio_3.cpp
+++ Linguagem de programação/aula_02/exercicio_3.cpp
@@ -4,17 +4,17 @@ Se formar, indique o tipo de triângulo:
 Isósceles dois lados iguais, escaleno todos os lados diferentes e equilátero todos os lados iguais.
 
 Considere:
-Para construir um triângulo é necessário que a medida de qualquer um dos lados seja menor que a soma das medidas dos outros dois e maior que o valor absoluto da diferença entre essas medidas.
+Para construir um triângulo é necessário que a medida de qualquer um dos lados
+seja menor que a soma das medidas dos outros dois 
+e maior que o valor absoluto da diferença entre essas medidas.
 
 */
 
-
 #include <iostream> 
 #include <windows.h>
+#include <math.h>
 
 using namespace std;
-
-
 
 int main() {
     int codigoBR = 65001;
@@ -28,10 +28,16 @@ int main() {
     cout << "Insira os lados do triângulo: \n";
     cin >> a >> b >> c;
 
-    soma = a + b + c;
-
-    if(soma > a && soma > b && soma > c)
+    if(a < (b + c) && a > fabs(b - c))
         triangulo = true;
+    else 
+        if(b < (a + c) && b > fabs(a - c))
+            triangulo = true;
+        else 
+            if (c < (a + b) && c > fabs(a - b))
+                triangulo = true;
+            else
+                triangulo = false;
 
     if(triangulo){
         if(a == b && b == c)
