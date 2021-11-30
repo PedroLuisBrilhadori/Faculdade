be91bc71b0ff87dae4a24fbb8cb0ed90198e1c8c
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-11-30 11:00:50 -0300

    feat: funcção encrypt e decrypt funcionando

--- linguagemdeprogramacao/arquivos/prova.cpp
+++ linguagemdeprogramacao/arquivos/prova.cpp
@@ -2,12 +2,37 @@
 #include <windows.h>
 #include <string.h>
 
+#define TAM 50
+
 using namespace std;
+
+void criptografar(char * texto, char * cripto, int tam){
+    for(int i = 0; i < tam; i++){
+        if(texto[i] != 32)
+            cripto[i] = texto[i] + 3;
+        else 
+            cripto[i] = texto[i];       
+    }
+}
+
+void descriptografar(char * cripto, int tam){
+    for(int i = 0; i < tam; i++){
+        if(cripto[i] != 32)
+            cripto[i] = cripto[i] - 3;
+    }
+}
+
+void imprimir (char * imprime, int fim){
+    for(int i = 0; i <= fim; i++){
+        cout << imprime[i];
+    }
+    cout << "\n";
+}
 
 int main(){
     system("cls");
 
-    char texto[50], aux, fim = 0;
+    char texto[TAM], cripto[TAM], aux, fim = 0;
 
     FILE *arquivo;
     arquivo = fopen("texto.txt","r");
@@ -23,9 +48,11 @@ int main(){
         }
     }
 
-    for(int i = 0; i <= fim; i++){
-        cout << texto[i];
-    }
+    criptografar(texto, cripto, fim);
+    imprimir(cripto, fim);
+
+    descriptografar(cripto, fim);
+    imprimir(cripto, fim);
 
     return 0;
 }
