ca06906f63bce83f17e3c23867a43b08977957a2
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-03-24 10:16:17 -0300

    feat: adiciona 1 item do exercicio

--- estrutura-d-dados/filas/exfila/Fila.cpp
+++ estrutura-d-dados/filas/exfila/Fila.cpp
@@ -44,83 +44,12 @@ bool Fila::remover(int &x) {
 
 
 bool Fila::primeiro(int &x) {
-	// caso a lista esteja vazia retornar falso
-	// retornar em x o valor do primeiro elemento da lista
-	// seu c�digo aqui:
-	
-	
-	
-}
+	if(vazia()){
+		return false;
+	}
 
-bool Fila::ultimo(int &x) {
-	// caso a lista esteja vazia retornar falso
-	// retornar em x o valor do �ltimo elemento da lista
-	// seu c�digo aqui:
-
-
-	
-}
-
-bool Fila::esvaziar() {
-	// caso a lista esteja vazia retornar falso
-	// remover todos os elementos da lista
-	// seu c�digo aqui:
-	
-	
-}
-
-int Fila::tamanho() {
-	// retornar o tamanho da lista, 0 se estiver vazia
-	// seu c�digo aqui:
-	
-
-	
-}
-
-bool Fila::inverter() {
-	// caso a lista esteja vazia retornar falso
-	// inverter a ordem dos elementos da lista
-	// seu c�digo aqui:
-	
-	
-	
-}
-
-string Fila::listar() {
-	// retornar string com todos os elementos da lista separados por ;
-	// exemplo: 1 ; 3 ; 5
-	// string vazia se a lista estiver vazia
-	// seu c�digo aqui:
-
-
-
-}
-
-bool Fila::estaNaFila(int x) {
-	// retornar verdadeiro caso o valor x seja localizado na fila
-	// seu c�digo aqui:
-	
-	
-}
-
-Fila Fila::interseccao(Fila auxFila) {
-	// retornar objeto Fila contendo a intersec��o com a lista atual
-	// elementos em comum com as 2 listas
-	// sem repeti��o de elementos
-	// seu c�digo aqui:
-		
-
-	
-}
-
-Fila Fila::uniao(Fila auxFila) {
-	// retornar objeto Fila contendo a uni�o com a lista atual
-	// elementos das 2 listas
-	// sem repeti��o de elementos
-	// seu c�digo aqui:
-
-
-	
+	x = inicio->valor;
+	return true;
 }
 
 
--- estrutura-d-dados/filas/exfila/teste01.cpp
+++ estrutura-d-dados/filas/exfila/teste01.cpp
@@ -2,18 +2,18 @@
 #include "Fila.cpp"
 
 
-//	Altere a classe que implementa a fila adicionando os seguintes métodos:
-//  a.	Um método primeiro que deve retornar o primeiro elemento da fila;
-//  b.	Um método último que deve retornar o último elemento da fila;
-//  c.	Um método esvaziar que remova todos os elementos da fila;
-//  d.	Um método tamanho que retorne o tamanho atual da fila;
-//  e.	Um método inverter que inverta a ordem dos elementos atuais da fila;
-//  f.	Um método listar que retorne um vetor contendo todos os elementos da fila;
-//  g.	Um método que verifica se um dado valor se encontra na fila;
-//  h.	Um método que receba uma lista e retorne a intersecção com essa lista;
-//  i.	Um método que receba uma lista e retorne a união com essa lista;
+//	Altere a classe que implementa a fila adicionando os seguintes mï¿½todos:
+//  a.	Um mï¿½todo primeiro que deve retornar o primeiro elemento da fila;
+//  b.	Um mï¿½todo ï¿½ltimo que deve retornar o ï¿½ltimo elemento da fila;
+//  c.	Um mï¿½todo esvaziar que remova todos os elementos da fila;
+//  d.	Um mï¿½todo tamanho que retorne o tamanho atual da fila;
+//  e.	Um mï¿½todo inverter que inverta a ordem dos elementos atuais da fila;
+//  f.	Um mï¿½todo listar que retorne um vetor contendo todos os elementos da fila;
+//  g.	Um mï¿½todo que verifica se um dado valor se encontra na fila;
+//  h.	Um mï¿½todo que receba uma lista e retorne a intersecï¿½ï¿½o com essa lista;
+//  i.	Um mï¿½todo que receba uma lista e retorne a uniï¿½o com essa lista;
 //
-// Implementar utilizando sempre somente os metódos inserir e remover.
+// Implementar utilizando sempre somente os metï¿½dos inserir e remover.
 
 
 using namespace std;
@@ -22,17 +22,12 @@ int main() {
     Fila minhaFila, fila01, fila02;
     int x, y;
 
-    if (minhaFila.vazia()) {
-        cout << "Esta vazia!\n";
-    } 
     minhaFila.inserir(1);
-    cout << "Esta vazia: " << minhaFila.vazia() << "\n";
     minhaFila.inserir(2);
-    cout << "Esta vazia: " << minhaFila.vazia() << "\n";
-    minhaFila.remover(x);
-    cout << "Esta vazia: " << minhaFila.vazia() << "\n";
-    minhaFila.remover(x);
-    cout << "Esta vazia: " << minhaFila.vazia() << "\n";
+
+    minhaFila.primeiro(x);
+    cout << "Primeiro item: " << x << endl;
+
     
     //fila02 = minhaFila.interseccao(fila01);
     //cout << fila02.listar();
